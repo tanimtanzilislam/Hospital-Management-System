3aed5c006f23982df3aefd5773c02e6156e2240f
Tanim <tanimtanzilislam9@gmail.com>
2025-09-07 19:20:18 +0600

    added all doctor features

--- .history/Database/hmisphp_20250716220257.sql
+++ .history/Database/hmisphp_20250716220257.sql
@@ -1,4 +1,11 @@
-
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 01, 2022 at 11:03 AM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
